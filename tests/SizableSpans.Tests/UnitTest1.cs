8f50d4cc3a9cd2ea53d7f947997cb6b5e89589e8
zhouyuelin <as.zhouyuelin@aisino.com>
2025-03-23 18:26:44 +0800

    Add SizableSpans.Tests.csproj

---
+++ tests/SizableSpans.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace SizableSpans.Tests {
+    public class UnitTest1 {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
