d2ae8926a8b43753d3483a6388d9ad1cd761ce7d
zhouyuelin <as.zhouyuelin@aisino.com>
2025-03-30 22:58:39 +0800

    SizableSpans: Add ISizableSpanProvider, IReadOnlySizableSpanProvider interfaces.

---
+++ src/SizableSpans.Core/IReadOnlySizableSpanProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Zyl.SizableSpans {
+    /// <summary>
+    /// The provider interface of read only sizable range span (只读大范围跨距的提供者接口).
+    /// </summary>
+    /// <typeparam name="T">The element type (元素的类型).</typeparam>
+    interface IReadOnlySizableSpanProvider<T> : IReadOnlySizableSpanBase<T> {
+
+        /// <summary>
+        /// Create a <see cref="ReadOnlySizableSpan{T}"/> using the specified type (使用指定类型来创建 <see cref="ReadOnlySizableSpan{T}"/>).
+        /// </summary>
+        /// <typeparam name="TTo">The specified type (指定类型).</typeparam>
+        /// <returns>a <see cref="ReadOnlySizableSpan{T}"/></returns>
+        public ReadOnlySizableSpan<TTo> CreateReadOnlySizableSpan<TTo>();
+
+    }
+}
---
+++ src/SizableSpans.Core/ISizableSpanProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Zyl.SizableSpans {
+    /// <summary>
+    /// The provider interface of sizable range span (大范围跨距的提供者接口).
+    /// </summary>
+    /// <typeparam name="T">The element type (元素的类型).</typeparam>
+    interface ISizableSpanProvider<T> : ISizableSpanBase<T> {
+
+        /// <summary>
+        /// Create a <see cref="SizableSpan{T}"/> using the specified type (使用指定类型来创建 <see cref="SizableSpan{T}"/>).
+        /// </summary>
+        /// <typeparam name="TTo">The specified type (指定类型).</typeparam>
+        /// <returns>a <see cref="SizableSpan{T}"/></returns>
+        public SizableSpan<TTo> CreateSizableSpan<TTo>();
+
+    }
+}
