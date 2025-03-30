f15691c9bfd689c15f4961bdd4e71742d5b82a86
zhouyuelin <as.zhouyuelin@aisino.com>
2025-03-30 13:21:23 +0800

    Add project: SizableSpans.Benchmarks.NetFw

---
+++ tests/SizableSpans.Benchmarks.NetFw/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zyl.SizableSpans.Benchmarks.NetFw {
+    class Program {
+        static void Main(string[] args) {
+            TextWriter writer = Console.Out;
+            writer.WriteLine("SizableSpans.Benchmarks.NetFw");
+        }
+    }
+}
