f6b8fe7ebae591ff1c0671514b22400e964ed3b5
zhouyuelin <as.zhouyuelin@aisino.com>
2025-04-01 23:57:19 +0800

    SizableSpans.Core: SizableSpanExtensions define ItemsToString overload methods.

--- src/SizableSpans.Core/SizableSpanExtensions.Sizable.cs
+++ src/SizableSpans.Core/SizableSpanExtensions.Sizable.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿#if NET9_0_OR_GREATER
+#define STRUCT_REF_INTERFACE // C# 13 - ref struct interface; allows ref struct. https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-13#ref-struct-interfaces
+#endif // NET9_0_OR_GREATER
+
+using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -15,6 +19,17 @@ namespace Zyl.SizableSpans {
         /// <returns>a <see cref="ReadOnlySizableSpan{T}"/></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ReadOnlySizableSpan<T> AsReadOnlySizableSpan<T>(this ReadOnlySpan<T> span) {
+            return (ReadOnlySizableSpan<T>)span;
+        }
+
+        /// <summary>
+        /// An conversion of a <see cref="SizableSpan{T}"/> to a <see cref="ReadOnlySizableSpan{T}"/> (<see cref="SizableSpan{T}"/> 到 <see cref="ReadOnlySizableSpan{T}"/> 的转换).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="span">The object to convert (要转换的对象).</param>
+        /// <returns>a <see cref="ReadOnlySizableSpan{T}"/></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ReadOnlySizableSpan<T> AsReadOnlySizableSpan<T>(this SizableSpan<T> span) {
             return (ReadOnlySizableSpan<T>)span;
         }
 
@@ -118,6 +133,149 @@ namespace Zyl.SizableSpans {
         public static bool IsEmpty(this ISizableLength source) {
             return (TSize)0 == source.Length;
         }
+
+        /// <summary>
+        /// Convert items data into string. The headerLength parameter uses the value of <see cref="SizableMemoryMarshal.SpanViewLength"/> (将各项数据转为字符串. headerLength 参数使用 <see cref="SizableMemoryMarshal.SpanViewLength"/> 的值).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The source data (源数据).</param>
+        /// <param name="noPrintType">Is no print type name (不打印类型名称).</param>
+        /// <returns>A formatted string (格式化后的字符串).</returns>
+        public static string ItemsToString<T>(this ReadOnlySizableSpan<T> source, bool noPrintType = false) {
+            return ItemsToString(source, (TSize)SizableMemoryMarshal.SpanViewLength, default, noPrintType);
+        }
+
+        /// <summary>
+        /// Convert items data into string. It has the <paramref name="headerLength"/>, <paramref name="footerLength"/> parameter (将各项数据转为字符串. 它具有 <paramref name="headerLength"/>, <paramref name="footerLength"/> 参数).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The source data (源数据).</param>
+        /// <param name="headerLength">The max length of header data (头部的最大长度).</param>
+        /// <param name="footerLength">The max length of footer data (尾部的最大长度).</param>
+        /// <param name="noPrintType">Is no print type name (不打印类型名称).</param>
+        /// <returns>A formatted string (格式化后的字符串).</returns>
+        public static string ItemsToString<T>(this ReadOnlySizableSpan<T> source, TSize headerLength, TSize footerLength = default, bool noPrintType = false) {
+            return ItemsToStringUnsafe(in source.GetPinnableReference(), source.Length, "Zyl.SizableSpans.ReadOnlySizableSpan", headerLength, footerLength, noPrintType);
+        }
+
+        /// <inheritdoc cref="ItemsToString{T}(ReadOnlySizableSpan{T}, bool)"/>
+        public static string ItemsToString<T>(this SizableSpan<T> source, bool noPrintType = false) {
+            return ItemsToString(source, (TSize)SizableMemoryMarshal.SpanViewLength, default, noPrintType);
+        }
+
+        /// <inheritdoc cref="ItemsToString{T}(ReadOnlySizableSpan{T}, nuint, nuint, bool)"/>
+        public static string ItemsToString<T>(this SizableSpan<T> source, TSize headerLength, TSize footerLength = default, bool noPrintType = false) {
+            return ItemsToStringUnsafe(ref source.GetPinnableReference(), source.Length, "Zyl.SizableSpans.SizableSpan", headerLength, footerLength, noPrintType);
+        }
+
+#if STRUCT_REF_INTERFACE
+
+        //Output.WriteLine("TSpan without typeSample: {0}", span.ItemsToString()); // CS0411 The type arguments for method 'SizableSpanExtensions.ItemsToString<T, TSpan>(TSpan, bool)' cannot be inferred from the usage. Try specifying the type arguments explicitly.
+        //Output.WriteLine("TSpan without typeSample: {0}", span.ItemsToString<int, SizableSpan<int>>()); // OK. But the code is too long. So it was decided to disable it.
+        //public static string ItemsToString<T, TSpan>(this TSpan span, bool noPrintType = false)
+        //        where TSpan : IReadOnlySizableSpanBase<T>, allows ref struct {
+        //    return ItemsToString(span, span.GetPinnableReadOnlyReference(), noPrintType);
+        //}
+
+        /// <summary>
+        /// Convert items data into string. The headerLength parameter uses the value of <see cref="SizableMemoryMarshal.SpanViewLength"/> (将各项数据转为字符串. headerLength 参数使用 <see cref="SizableMemoryMarshal.SpanViewLength"/> 的值).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <typeparam name="TSpan">The type of span (跨度的类型).</typeparam>
+        /// <param name="source">The source data (源数据).</param>
+        /// <param name="typeSample">Sample of type. Only its type is referenced, not its data. (类型的样例. 仅参考它的类型，不使用它的数据).</param>
+        /// <param name="noPrintType">Is no print type name (不打印类型名称).</param>
+        /// <returns>A formatted string (格式化后的字符串).</returns>
+        public static string ItemsToString<T, TSpan>(this TSpan source, in T typeSample, bool noPrintType = false)
+                where TSpan : IReadOnlySizableSpanBase<T>, allows ref struct {
+            return ItemsToString(source, in typeSample, (TSize)SizableMemoryMarshal.SpanViewLength, default, noPrintType);
+        }
+
+        /// <summary>
+        /// Convert items data into string. It has the <paramref name="headerLength"/>, <paramref name="footerLength"/> parameter (将各项数据转为字符串. 它具有 <paramref name="headerLength"/>, <paramref name="footerLength"/> 参数).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <typeparam name="TSpan">The type of span (跨度的类型).</typeparam>
+        /// <param name="source">The source data (源数据).</param>
+        /// <param name="typeSample">Sample of type. Only its type is referenced, not its data. (类型的样例. 仅参考它的类型，不使用它的数据).</param>
+        /// <param name="headerLength">The max length of header data (头部的最大长度).</param>
+        /// <param name="footerLength">The max length of footer data (尾部的最大长度).</param>
+        /// <param name="noPrintType">Is no print type name (不打印类型名称).</param>
+        /// <returns>A formatted string (格式化后的字符串).</returns>
+        public static string ItemsToString<T, TSpan>(this TSpan source, in T typeSample, TSize headerLength, TSize footerLength = default, bool noPrintType = false)
+                where TSpan : IReadOnlySizableSpanBase<T>, allows ref struct {
+            _ = typeSample;
+            string typeName = "Zyl.SizableSpans.SizableSpan";
+            return ItemsToStringUnsafe(in source.GetPinnableReadOnlyReference(), source.Length, typeName, headerLength, footerLength, noPrintType);
+        }
+
+#endif // STRUCT_REF_INTERFACE
+
+        /// <summary>
+        /// Unsafe convert items data into string. The headerLength parameter uses the value of <see cref="SizableMemoryMarshal.SpanViewLength"/> (非安全的将各项数据转为字符串. headerLength 参数使用 <see cref="SizableMemoryMarshal.SpanViewLength"/> 的值).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The reference to source data (源数据的引用).</param>
+        /// <param name="length">The length of source data (源数据的长度).</param>
+        /// <param name="typeName">The type name (类型名称).</param>
+        /// <param name="noPrintType">Is no print type name (不打印类型名称).</param>
+        /// <returns>A formatted string (格式化后的字符串).</returns>
+        internal static string ItemsToStringUnsafe<T>(ref readonly T source, TSize length, string typeName, bool noPrintType = false) {
+            return ItemsToStringUnsafe(in source, length, typeName, (TSize)SizableMemoryMarshal.SpanViewLength, default, noPrintType);
+        }
+
+        /// <summary>
+        /// Unsafe convert items data into string. It has the <paramref name="headerLength"/>, <paramref name="footerLength"/> parameter (非安全的将各项数据转为字符串. 它具有 <paramref name="headerLength"/>, <paramref name="footerLength"/> 参数).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The reference to source data (源数据的引用).</param>
+        /// <param name="length">The length of source data (源数据的长度).</param>
+        /// <param name="typeName">The type name (类型名称).</param>
+        /// <param name="headerLength">The max length of header data (头部的最大长度).</param>
+        /// <param name="footerLength">The max length of footer data (尾部的最大长度).</param>
+        /// <param name="noPrintType">Is no print type name (不打印类型名称).</param>
+        /// <returns>A formatted string (格式化后的字符串).</returns>
+        internal static string ItemsToStringUnsafe<T>(ref readonly T source, TSize length, string typeName, TSize headerLength, TSize footerLength = default, bool noPrintType = false) {
+            return ItemsToStringUnsafe_NoItems(in source, length, typeName, noPrintType);
+        }
+
+        private static string ItemsToStringUnsafe_NoItems<T>(ref readonly T source, TSize length, string typeName, bool noPrintType = false) {
+            string rt;
+            if (noPrintType) {
+                rt = $"{typeName}<{typeof(T).Name}>[{length}]";
+            } else {
+                rt = $"[{length}]";
+            }
+            return rt;
+        }
+
+        /// <summary>
+        /// Unsafe convert items data append to  string. The headerLength parameter uses the value of <see cref="SizableMemoryMarshal.SpanViewLength"/> (非安全的将各项数据转追加字符串. headerLength 参数使用 <see cref="SizableMemoryMarshal.SpanViewLength"/> 的值).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The reference to source data (源数据的引用).</param>
+        /// <param name="length">The length of source data (源数据的长度).</param>
+        /// <param name="typeName">The type name (类型名称).</param>
+        /// <param name="outputBuilder">The output <see cref="StringBuilder"/> (输出的 <see cref="StringBuilder"/>).</param>
+        /// <param name="noPrintType">Is no print type name (不打印类型名称).</param>
+        internal static void ItemsAppendStringUnsafe<T>(ref readonly T source, TSize length, string typeName, StringBuilder outputBuilder, bool noPrintType = false) {
+            ItemsAppendStringUnsafe(in source, length, typeName, outputBuilder, (TSize)SizableMemoryMarshal.SpanViewLength, default, noPrintType);
+        }
+
+        /// <summary>
+        /// Unsafe convert items data append to string. It has the <paramref name="headerLength"/>, <paramref name="footerLength"/> parameter (非安全的将各项数据追加字符串. 它具有 <paramref name="headerLength"/>, <paramref name="footerLength"/> 参数).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The reference to source data (源数据的引用).</param>
+        /// <param name="length">The length of source data (源数据的长度).</param>
+        /// <param name="typeName">The type name (类型名称).</param>
+        /// <param name="outputBuilder">The output <see cref="StringBuilder"/> (输出的 <see cref="StringBuilder"/>).</param>
+        /// <param name="headerLength">The max length of header data (头部的最大长度).</param>
+        /// <param name="footerLength">The max length of footer data (尾部的最大长度).</param>
+        /// <param name="noPrintType">Is no print type name (不打印类型名称).</param>
+        internal static void ItemsAppendStringUnsafe<T>(ref readonly T source, TSize length, string typeName, StringBuilder outputBuilder, TSize headerLength, TSize footerLength = default, bool noPrintType = false) {
+        }
+
 
     }
 }
---
+++ tests/SizableSpans.Tests/SizableSpanExtensionsTest.cs
@@ -0,0 +1,70 @@
+﻿#if NET9_0_OR_GREATER
+#define STRUCT_REF_INTERFACE // C# 13 - ref struct interface; allows ref struct. https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-13#ref-struct-interfaces
+#endif // NET9_0_OR_GREATER
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Zyl.SizableSpans.Tests {
+    public class SizableSpanExtensionsTest {
+        const int bufferSize = 16;
+
+        private readonly ITestOutputHelper _output;
+
+        public SizableSpanExtensionsTest(ITestOutputHelper output) {
+            _output = output;
+        }
+
+        private ITestOutputHelper Output => _output;
+
+        [Fact]
+        public void ItemsToStringTest() {
+            const nuint headerLength = (nuint)3;
+            const nuint footerLength = (nuint)4;
+            Span<int> sourceSpan = stackalloc int[bufferSize];
+            SizableSpan<int> span = sourceSpan; // Implicit conversion Span to SizableSpan.
+            span.Fill(1);
+            span[(nuint)0] = 0;
+            span[span.Length - 2] = 2;
+
+            // Output - span.
+            TestItem(span);
+
+            // Output - Empty.
+            TestItem(SizableSpan<long>.Empty);
+
+            // Output - TSpan.
+#if STRUCT_REF_INTERFACE
+            // Try without typeSample.
+            //Output.WriteLine("TSpan without typeSample: {0}", span.ItemsToString()); // CS0411 The type arguments for method 'SizableSpanExtensions.ItemsToString<T, TSpan>(TSpan, bool)' cannot be inferred from the usage. Try specifying the type arguments explicitly.
+            //Output.WriteLine("TSpan without typeSample: {0}", span.ItemsToString<int, SizableSpan<int>>()); // OK. But the code is too long. So it was decided to disable it.
+            // OK.
+            Output.WriteLine("TSpan with typeSample: {0}", span.ItemsToString(span.GetPinnableReference()));
+            Output.WriteLine("TSpan with typeSample and noPrintType: {0}", span.ItemsToString(span.GetPinnableReference(), headerLength, footerLength, false));
+            Output.WriteLine("TSpan with footerLength: {0}", span.ItemsToString(span.GetPinnableReference(), headerLength, footerLength));
+            Output.WriteLine("TSpan with footerLength and noPrintType: {0}", span.ItemsToString(span.GetPinnableReference(), headerLength, footerLength, false));
+#endif // STRUCT_REF_INTERFACE
+
+            void TestItem<T>(SizableSpan<T> span) {
+                // Output - SizableSpan.
+                Output.WriteLine("SizableSpan with typeSample: {0}", span.ItemsToString());
+                Output.WriteLine("SizableSpan with typeSample and noPrintType: {0}", span.ItemsToString(headerLength, footerLength, false));
+                Output.WriteLine("SizableSpan with footerLength: {0}", span.ItemsToString(headerLength, footerLength));
+                Output.WriteLine("SizableSpan with footerLength and noPrintType: {0}", span.ItemsToString(headerLength, footerLength, false));
+
+                // Output - ReadOnlySizableSpan.
+                ReadOnlySizableSpan<T> spanReadOnly = span;
+                Output.WriteLine("ReadOnlySizableSpan with typeSample: {0}", spanReadOnly.ItemsToString());
+                Output.WriteLine("ReadOnlySizableSpan with typeSample and noPrintType: {0}", spanReadOnly.ItemsToString(headerLength, footerLength, false));
+                Output.WriteLine("ReadOnlySizableSpan with footerLength: {0}", spanReadOnly.ItemsToString(headerLength, footerLength));
+                Output.WriteLine("ReadOnlySizableSpan with footerLength and noPrintType: {0}", spanReadOnly.ItemsToString(headerLength, footerLength, false));
+            }
+        }
+
+    }
+}
