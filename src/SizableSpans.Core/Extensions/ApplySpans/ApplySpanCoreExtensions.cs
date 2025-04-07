07988dc5da355596b5b2a7f386c294de6f582b9c
zhouyuelin <as.zhouyuelin@aisino.com>
2025-04-07 23:26:08 +0800

    SizableSpans.Core: Add ApplySpanCoreExtensions.

---
+++ src/SizableSpans.Core/Extensions/ApplySpans/ApplySpanCoreExtensions.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zyl.SizableSpans.Reflection;
+
+namespace Zyl.SizableSpans.Extensions.ApplySpans {
+
+    /// <summary>
+    /// Provides commonly used extension methods for the span-related types, such as <see cref="Span{T}"/> and <see cref="ReadOnlySpan{T}"/> (提供跨度相关的类型的常用的扩展方法，例如 <see cref="Span{T}"/> 和 <see cref="ReadOnlySpan{T}"/>).
+    /// </summary>
+    public static class ApplySpanCoreExtensions {
+
+        /// <summary>
+        /// Convert items data append string. The headerLength parameter uses the value of <see cref="SizableMemoryMarshal.SpanViewLength"/> (将各项数据转追加字符串. headerLength 参数使用 <see cref="SizableMemoryMarshal.SpanViewLength"/> 的值).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The source data (源数据).</param>
+        /// <param name="output">The output <see cref="StringBuilder"/> (输出的 <see cref="StringBuilder"/>).</param>
+        /// <param name="itemFormater">The formater of each item (各项的格式化器). Default value is <see cref="ItemFormaters.Default">ItemFormaters.Default</see>. Prototype is `string func(TSize index, T value)`.</param>
+        /// <param name="stringFlags">Flags for convert items data into string (各项数据转字符串的标志).</param>
+        /// <param name="nameFlags">Flags for type name (类型名的标志).</param>
+        /// <seealso cref="ItemFormaters"/>
+        public static void ItemsAppendString<T>(this ReadOnlySpan<T> source, StringBuilder output, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            ItemsAppendString(source, output, (TSize)SizableMemoryMarshal.SpanViewLength, default, itemFormater, stringFlags, nameFlags);
+        }
+
+        /// <summary>
+        /// Convert items data append string. It has the <paramref name="headerLength"/>, <paramref name="footerLength"/> parameter (将各项数据追加字符串. 它具有 <paramref name="headerLength"/>, <paramref name="footerLength"/> 参数).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The source data (源数据).</param>
+        /// <param name="output">The output <see cref="StringBuilder"/> (输出的 <see cref="StringBuilder"/>).</param>
+        /// <param name="headerLength">The max length of header data (头部的最大长度).</param>
+        /// <param name="footerLength">The max length of footer data (尾部的最大长度).</param>
+        /// <param name="itemFormater">The formater of each item (各项的格式化器). Default value is <see cref="ItemFormaters.Default">ItemFormaters.Default</see>. Prototype is `string func(TSize index, T value)`.</param>
+        /// <param name="stringFlags">Flags for convert items data into string (各项数据转字符串的标志).</param>
+        /// <param name="nameFlags">Flags for type name (类型名的标志).</param>
+        /// <seealso cref="ItemFormaters"/>
+        public static void ItemsAppendString<T>(this ReadOnlySpan<T> source, StringBuilder output, TSize headerLength, TSize footerLength = default, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            if (!stringFlags.HasFlag(ItemsToStringFlags.HideType)) {
+                TypeNameUtil.AppendName(output, typeof(ReadOnlySpan<T>), nameFlags, null, typeof(T));
+            }
+            SizableSpanExtensions.ItemsAppendStringUnsafe(in source.GetPinnableReference(), (TSize)source.Length, output, headerLength, footerLength, itemFormater, stringFlags);
+        }
+
+        /// <inheritdoc cref="ItemsAppendString{T}(ReadOnlySpan{T}, Func{nuint, T, string}?, ItemsToStringFlags, TypeNameFlags)"/>
+        public static void ItemsAppendString<T>(this Span<T> source, StringBuilder output, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            ItemsAppendString(source, output, (TSize)SizableMemoryMarshal.SpanViewLength, default, itemFormater, stringFlags, nameFlags);
+        }
+
+        /// <inheritdoc cref="ItemsAppendString{T}(ReadOnlySpan{T}, nuint, nuint, Func{nuint, T, string}?, ItemsToStringFlags, TypeNameFlags)"/>
+        public static void ItemsAppendString<T>(this Span<T> source, StringBuilder output, TSize headerLength, TSize footerLength = default, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            if (!stringFlags.HasFlag(ItemsToStringFlags.HideType)) {
+                TypeNameUtil.AppendName(output, typeof(Span<T>), nameFlags, null, typeof(T));
+            }
+            SizableSpanExtensions.ItemsAppendStringUnsafe(in source.GetPinnableReference(), (TSize)source.Length, output, headerLength, footerLength, itemFormater, stringFlags);
+        }
+
+
+        /// <summary>
+        /// Convert items data append string to action. The headerLength parameter uses the value of <see cref="SizableMemoryMarshal.SpanViewLength"/> (将各项数据转追加字符串到动作. headerLength 参数使用 <see cref="SizableMemoryMarshal.SpanViewLength"/> 的值).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The source data (源数据).</param>
+        /// <param name="output">The output action (输出动作).</param>
+        /// <param name="itemFormater">The formater of each item (各项的格式化器). Default value is <see cref="ItemFormaters.Default">ItemFormaters.Default</see>. Prototype is `string func(TSize index, T value)`.</param>
+        /// <param name="stringFlags">Flags for convert items data into string (各项数据转字符串的标志).</param>
+        /// <param name="nameFlags">Flags for type name (类型名的标志).</param>
+        /// <seealso cref="ItemFormaters"/>
+        public static void ItemsAppendStringTo<T>(this ReadOnlySpan<T> source, Action<string> output, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            ItemsAppendStringTo(source, output, (TSize)SizableMemoryMarshal.SpanViewLength, default, itemFormater, stringFlags, nameFlags);
+        }
+
+        /// <summary>
+        /// Convert items data append string to action. It has the <paramref name="headerLength"/>, <paramref name="footerLength"/> parameter (将各项数据转追加字符串到动作. 它具有 <paramref name="headerLength"/>, <paramref name="footerLength"/> 参数).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The source data (源数据).</param>
+        /// <param name="output">The output action (输出动作).</param>
+        /// <param name="headerLength">The max length of header data (头部的最大长度).</param>
+        /// <param name="footerLength">The max length of footer data (尾部的最大长度).</param>
+        /// <param name="itemFormater">The formater of each item (各项的格式化器). Default value is <see cref="ItemFormaters.Default">ItemFormaters.Default</see>. Prototype is `string func(TSize index, T value)`.</param>
+        /// <param name="stringFlags">Flags for convert items data into string (各项数据转字符串的标志).</param>
+        /// <param name="nameFlags">Flags for type name (类型名的标志).</param>
+        /// <seealso cref="ItemFormaters"/>
+        public static void ItemsAppendStringTo<T>(this ReadOnlySpan<T> source, Action<string> output, TSize headerLength, TSize footerLength = default, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            if (!stringFlags.HasFlag(ItemsToStringFlags.HideType)) {
+                TypeNameUtil.AppendNameTo(output, typeof(ReadOnlySpan<T>), nameFlags, null, typeof(T));
+            }
+            SizableSpanExtensions.ItemsAppendStringToUnsafe(in source.GetPinnableReference(), (TSize)source.Length, output, headerLength, footerLength, itemFormater, stringFlags);
+        }
+
+        /// <inheritdoc cref="ItemsAppendStringTo{T}(ReadOnlySpan{T}, Func{nuint, T, string}?, ItemsToStringFlags, TypeNameFlags)"/>
+        public static void ItemsAppendStringTo<T>(this Span<T> source, Action<string> output, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            ItemsAppendStringTo(source, output, (TSize)SizableMemoryMarshal.SpanViewLength, default, itemFormater, stringFlags, nameFlags);
+        }
+
+        /// <inheritdoc cref="ItemsAppendStringTo{T}(ReadOnlySpan{T}, nuint, nuint, Func{nuint, T, string}?, ItemsToStringFlags, TypeNameFlags)"/>
+        public static void ItemsAppendStringTo<T>(this Span<T> source, Action<string> output, TSize headerLength, TSize footerLength = default, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            if (!stringFlags.HasFlag(ItemsToStringFlags.HideType)) {
+                TypeNameUtil.AppendNameTo(output, typeof(Span<T>), nameFlags, null, typeof(T));
+            }
+            SizableSpanExtensions.ItemsAppendStringToUnsafe(in source.GetPinnableReference(), (TSize)source.Length, output, headerLength, footerLength, itemFormater, stringFlags);
+        }
+
+        /// <summary>
+        /// Convert items data into string. The headerLength parameter uses the value of <see cref="SizableMemoryMarshal.SpanViewLength"/> (将各项数据转为字符串. headerLength 参数使用 <see cref="SizableMemoryMarshal.SpanViewLength"/> 的值).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The source data (源数据).</param>
+        /// <param name="itemFormater">The formater of each item (各项的格式化器). Default value is <see cref="ItemFormaters.Default">ItemFormaters.Default</see>. Prototype is `string func(TSize index, T value)`.</param>
+        /// <param name="stringFlags">Flags for convert items data into string (各项数据转字符串的标志).</param>
+        /// <param name="nameFlags">Flags for type name (类型名的标志).</param>
+        /// <returns>A formatted string (格式化后的字符串).</returns>
+        /// <seealso cref="ItemFormaters"/>
+        public static string ItemsToString<T>(this ReadOnlySpan<T> source, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            return ItemsToString(source, (TSize)SizableMemoryMarshal.SpanViewLength, default, itemFormater, stringFlags, nameFlags);
+        }
+
+        /// <summary>
+        /// Convert items data into string. It has the <paramref name="headerLength"/>, <paramref name="footerLength"/> parameter (将各项数据转为字符串. 它具有 <paramref name="headerLength"/>, <paramref name="footerLength"/> 参数).
+        /// </summary>
+        /// <typeparam name="T">The element type (元素的类型).</typeparam>
+        /// <param name="source">The source data (源数据).</param>
+        /// <param name="headerLength">The max length of header data (头部的最大长度).</param>
+        /// <param name="footerLength">The max length of footer data (尾部的最大长度).</param>
+        /// <param name="itemFormater">The formater of each item (各项的格式化器). Default value is <see cref="ItemFormaters.Default">ItemFormaters.Default</see>. Prototype is `string func(TSize index, T value)`.</param>
+        /// <param name="stringFlags">Flags for convert items data into string (各项数据转字符串的标志).</param>
+        /// <param name="nameFlags">Flags for type name (类型名的标志).</param>
+        /// <returns>A formatted string (格式化后的字符串).</returns>
+        /// <seealso cref="ItemFormaters"/>
+        public static string ItemsToString<T>(this ReadOnlySpan<T> source, TSize headerLength, TSize footerLength = default, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            StringBuilder stringBuilder = new StringBuilder();
+            ItemsAppendString(source, stringBuilder, headerLength, footerLength, itemFormater, stringFlags);
+            return stringBuilder.ToString();
+        }
+
+        /// <inheritdoc cref="ItemsToString{T}(ReadOnlySpan{T}, Func{nuint, T, string}?, ItemsToStringFlags, TypeNameFlags)"/>
+        public static string ItemsToString<T>(this Span<T> source, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            return ItemsToString(source, (TSize)SizableMemoryMarshal.SpanViewLength, default, itemFormater, stringFlags, nameFlags);
+        }
+
+        /// <inheritdoc cref="ItemsToString{T}(ReadOnlySpan{T}, nuint, nuint, Func{nuint, T, string}?, ItemsToStringFlags, TypeNameFlags)"/>
+        public static string ItemsToString<T>(this Span<T> source, TSize headerLength, TSize footerLength = default, Func<TSize, T, string>? itemFormater = null, ItemsToStringFlags stringFlags = ItemsToStringFlags.Default, TypeNameFlags nameFlags = TypeNameFlags.Default) {
+            StringBuilder stringBuilder = new StringBuilder();
+            ItemsAppendString(source, stringBuilder, headerLength, footerLength, itemFormater, stringFlags);
+            return stringBuilder.ToString();
+        }
+
+    }
+
+}
---
+++ tests/SizableSpans.Tests/Extensions/ApplySpans/ApplySpanCoreExtensionsTest.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using Zyl.SizableSpans.Extensions.ApplySpans;
+using Zyl.SizableSpans.Reflection;
+
+namespace Zyl.SizableSpans.Tests.Extensions.ApplySpans {
+    public class ApplySpanCoreExtensionsTest {
+        const int bufferSize = 16;
+
+        private readonly ITestOutputHelper _output;
+
+        public ApplySpanCoreExtensionsTest(ITestOutputHelper output) {
+            _output = output;
+        }
+
+        private ITestOutputHelper Output => _output;
+
+        [Fact]
+        public void ItemsAppendStringToTest() {
+            const ItemsToStringFlags stringFlagsDefault = ItemsToStringFlags.Default;
+            const ItemsToStringFlags stringFlags = ItemsToStringFlags.HideType;
+            const TypeNameFlags nameFlags = TypeNameFlags.ShowNamespace | TypeNameFlags.SubShowNamespace;
+            const nuint headerLength = (nuint)3;
+            const nuint footerLength = (nuint)4;
+            string newLine = Environment.NewLine;
+            StringBuilder stringBuilder = new StringBuilder();
+            Action<string> output = delegate (string str) {
+                stringBuilder.Append(str);
+            };
+            Span<int> span = stackalloc int[bufferSize];
+            span.Fill(1);
+            span[0] = 0;
+            span[span.Length - 2] = 2;
+
+            // Output - Span.
+            CallSpan(output, span);
+
+            // Output - Empty.
+            CallSpan(output, Span<long>.Empty);
+
+            // done.
+            Output.WriteLine(stringBuilder.ToString());
+
+            void CallSpan<T>(Action<string> output, Span<T> span) {
+                // Output - Span.
+                output("Span: "); span.ItemsAppendStringTo(output); output(newLine);
+                output("Span with stringFlags: "); span.ItemsAppendStringTo(output, null, stringFlags); output(newLine);
+                output("Span with footerLength: "); span.ItemsAppendStringTo(output, headerLength, footerLength); output(newLine);
+                output("Span with footerLength and stringFlags: "); span.ItemsAppendStringTo(output, headerLength, footerLength, null, stringFlags); output(newLine);
+
+                // Output - ReadOnlySpan.
+                ReadOnlySpan<T> spanReadOnly = span;
+                output("ReadOnlySpan: "); spanReadOnly.ItemsAppendStringTo(output); output(newLine);
+                output("ReadOnlySpan with stringFlags: "); spanReadOnly.ItemsAppendStringTo(output, null, stringFlags); output(newLine);
+                output("ReadOnlySpan with nameFlags: "); spanReadOnly.ItemsAppendStringTo(output, null, stringFlagsDefault, nameFlags); output(newLine);
+                output("ReadOnlySpan with footerLength: "); spanReadOnly.ItemsAppendStringTo(output, headerLength, footerLength); output(newLine);
+                output("ReadOnlySpan with footerLength and stringFlags: "); spanReadOnly.ItemsAppendStringTo(output, headerLength, footerLength, null, stringFlags); output(newLine);
+            }
+        }
+
+        [Fact]
+        public void ItemsAppendStringTest() {
+            const ItemsToStringFlags stringFlagsDefault = ItemsToStringFlags.Default;
+            const ItemsToStringFlags stringFlags = ItemsToStringFlags.HideType;
+            const TypeNameFlags nameFlags = TypeNameFlags.ShowNamespace | TypeNameFlags.SubShowNamespace;
+            const nuint headerLength = (nuint)3;
+            const nuint footerLength = (nuint)4;
+            StringBuilder output = new StringBuilder();
+            Span<int> span = stackalloc int[bufferSize];
+            span.Fill(1);
+            span[0] = 0;
+            span[span.Length - 2] = 2;
+
+            // Output - Span.
+            CallSpan(output, span);
+
+            // Output - Empty.
+            CallSpan(output, Span<long>.Empty);
+
+            // done.
+            Output.WriteLine(output.ToString());
+
+            static void CallSpan<T>(StringBuilder output, Span<T> span) {
+                // Output - Span.
+                output.Append("Span: "); span.ItemsAppendString(output); output.AppendLine();
+                output.Append("Span with stringFlags: "); span.ItemsAppendString(output, null, stringFlags); output.AppendLine();
+                output.Append("Span with footerLength: "); span.ItemsAppendString(output, headerLength, footerLength); output.AppendLine();
+                output.Append("Span with footerLength and stringFlags: "); span.ItemsAppendString(output, headerLength, footerLength, null, stringFlags); output.AppendLine();
+
+                // Output - ReadOnlySpan.
+                ReadOnlySpan<T> spanReadOnly = span;
+                output.Append("ReadOnlySpan: "); spanReadOnly.ItemsAppendString(output); output.AppendLine();
+                output.Append("ReadOnlySpan with stringFlags: "); spanReadOnly.ItemsAppendString(output, null, stringFlags); output.AppendLine();
+                output.Append("ReadOnlySpan with nameFlags: "); spanReadOnly.ItemsAppendString(output, null, stringFlagsDefault, nameFlags); output.AppendLine();
+                output.Append("ReadOnlySpan with footerLength: "); spanReadOnly.ItemsAppendString(output, headerLength, footerLength); output.AppendLine();
+                output.Append("ReadOnlySpan with footerLength and stringFlags: "); spanReadOnly.ItemsAppendString(output, headerLength, footerLength, null, stringFlags); output.AppendLine();
+            }
+        }
+
+        [Fact]
+        public void ItemsToStringTest() {
+            const ItemsToStringFlags stringFlagsDefault = ItemsToStringFlags.Default;
+            const ItemsToStringFlags stringFlags = ItemsToStringFlags.HideType;
+            const TypeNameFlags nameFlags = TypeNameFlags.ShowNamespace | TypeNameFlags.SubShowNamespace;
+            const nuint headerLength = (nuint)3;
+            const nuint footerLength = (nuint)4;
+            Span<int> span = stackalloc int[bufferSize];
+            span.Fill(1);
+            span[0] = 0;
+            span[span.Length - 2] = 2;
+
+            // Output - Span.
+            CallSpan(span);
+
+            // Output - Empty.
+            CallSpan(Span<long>.Empty);
+
+            void CallSpan<T>(Span<T> span) {
+                // Output - Span.
+                Output.WriteLine("Span: {0}", span.ItemsToString());
+                Output.WriteLine("Span with stringFlags: {0}", span.ItemsToString(null, stringFlags));
+                Output.WriteLine("Span with footerLength: {0}", span.ItemsToString(headerLength, footerLength));
+                Output.WriteLine("Span with footerLength and stringFlags: {0}", span.ItemsToString(headerLength, footerLength, null, stringFlags));
+
+                // Output - ReadOnlySpan.
+                ReadOnlySpan<T> spanReadOnly = span;
+                Output.WriteLine("ReadOnlySpan: {0}", spanReadOnly.ItemsToString());
+                Output.WriteLine("ReadOnlySpan with stringFlags: {0}", spanReadOnly.ItemsToString(null, stringFlags));
+                Output.WriteLine("ReadOnlySpan with nameFlags: {0}", spanReadOnly.ItemsToString(null, stringFlagsDefault, nameFlags));
+                Output.WriteLine("ReadOnlySpan with footerLength: {0}", spanReadOnly.ItemsToString(headerLength, footerLength));
+                Output.WriteLine("ReadOnlySpan with footerLength and stringFlags: {0}", spanReadOnly.ItemsToString(headerLength, footerLength, null, stringFlags));
+            }
+        }
+
+    }
+}
