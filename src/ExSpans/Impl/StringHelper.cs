f01beab040df2f2bc88aebaedf817407ede3ca53
zhouyuelin <as.zhouyuelin@aisino.com>
2025-04-28 16:34:57 +0800

    ExSpans: Add StringHelper class, add CheckStringComparison method.

---
+++ src/ExSpans/Impl/StringHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zyl.ExSpans.Impl {
+    /// <summary>
+    /// <see cref="String"/> Helper.
+    /// </summary>
+    public static class StringHelper {
+
+        /// <summary>
+        /// Check <see cref="StringComparison"/>.
+        /// </summary>
+        /// <param name="comparisonType">Source comparisonType.</param>
+        public static void CheckStringComparison(StringComparison comparisonType) {
+            // Single comparison to check if comparisonType is within [CurrentCulture .. OrdinalIgnoreCase]
+            if ((uint)comparisonType > (uint)StringComparison.OrdinalIgnoreCase) {
+                throw new ArgumentOutOfRangeException(nameof(comparisonType));
+            }
+        }
+
+    }
+}
