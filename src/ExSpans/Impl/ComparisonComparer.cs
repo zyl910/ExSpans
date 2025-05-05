5b8adb6641e305ba649869464a217fc7262f1484
zhouyuelin <as.zhouyuelin@aisino.com>
2025-05-05 17:20:14 +0800

    ExSpans: Add ComparisonComparer class.

---
+++ src/ExSpans/Impl/ComparisonComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zyl.ExSpans.Impl {
+
+    internal sealed class ComparisonComparer<T> : Comparer<T> {
+        private readonly Comparison<T> _comparison;
+
+        public ComparisonComparer(Comparison<T> comparison) {
+            _comparison = comparison;
+        }
+
+        public override int Compare(T? x, T? y) => _comparison(x!, y!);
+    }
+
+}
