4c5f74a910e79ce9ab0eedaaea2966a2ff2fb105
zhouyuelin <as.zhouyuelin@aisino.com>
2025-05-15 22:46:25 +0800

    ExSpans: `ExSpanHelpers.T.Types` Add AggressiveInlining.

--- src/ExSpans/ExSpanHelpers.T.Types.cs
+++ src/ExSpans/ExSpanHelpers.T.Types.cs
@@ -125,29 +125,33 @@ namespace Zyl.ExSpans {
             //    return TVector.EqualsAny(left, right);
             //}
 #if NETCOREAPP3_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 bool HasMatch(Vector128<T> left, Vector128<T> right) {
                 return Vector128Helper.EqualsAny(left, right);
             }
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 bool HasMatch(Vector256<T> left, Vector256<T> right) {
                 return Vector256Helper.EqualsAny(left, right);
             }
 #endif // NETCOREAPP3_0_OR_GREATER
 #if NET8_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static bool HasMatch(Vector512<T> left, Vector512<T> right) {
                 return Vector512.EqualsAny(left, right);
             }
 #endif // NET8_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 bool HasMatch(Vector<T> left, Vector<T> right) {
                 return Vector.EqualsAny(left, right);
@@ -158,29 +162,33 @@ namespace Zyl.ExSpans {
             //    return TVector.Equals(left, right);
             //}
 #if NETCOREAPP3_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 Vector128<T> GetMatchMask(Vector128<T> left, Vector128<T> right) {
                 return Vector128Helper.Equals(left, right);
             }
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 Vector256<T> GetMatchMask(Vector256<T> left, Vector256<T> right) {
                 return Vector256Helper.Equals(left, right);
             }
 #endif // NETCOREAPP3_0_OR_GREATER
 #if NET8_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static Vector512<T> GetMatchMask(Vector512<T> left, Vector512<T> right) {
                 return Vector512.Equals(left, right);
             }
 #endif // NET8_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 Vector<T> GetMatchMask(Vector<T> left, Vector<T> right) {
                 return Vector.Equals(left, right);
@@ -197,23 +205,27 @@ namespace Zyl.ExSpans {
             //public static Vector128<T> NegateIfNeeded(Vector128<T> equals) => ~equals;
             //public static Vector256<T> NegateIfNeeded(Vector256<T> equals) => ~equals;
 #if NETCOREAPP3_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 Vector128<T> NegateIfNeeded(Vector128<T> equals) => Vector128s.OnesComplement(equals);
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 Vector256<T> NegateIfNeeded(Vector256<T> equals) => Vector256s.OnesComplement(equals);
 #endif // NETCOREAPP3_0_OR_GREATER
 #if NET8_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static Vector512<T> NegateIfNeeded(Vector512<T> equals) => ~equals;
 #endif // NET8_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 Vector<T> NegateIfNeeded(Vector<T> equals) => ~equals;
 
@@ -225,29 +237,33 @@ namespace Zyl.ExSpans {
             //    return !TVector.EqualsAll(left, right);
             //}
 #if NETCOREAPP3_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 bool HasMatch(Vector128<T> left, Vector128<T> right) {
                 return !Vector128Helper.EqualsAll(left, right);
             }
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 bool HasMatch(Vector256<T> left, Vector256<T> right) {
                 return !Vector256Helper.EqualsAll(left, right);
             }
 #endif // NETCOREAPP3_0_OR_GREATER
 #if NET8_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static bool HasMatch(Vector512<T> left, Vector512<T> right) {
                 return !Vector512.EqualsAll(left, right);
             }
 #endif // NET8_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 bool HasMatch(Vector<T> left, Vector<T> right) {
                 return !Vector.EqualsAll(left, right);
@@ -258,29 +274,33 @@ namespace Zyl.ExSpans {
             //    return ~TVector.Equals(left, right);
             //}
 #if NETCOREAPP3_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 Vector128<T> GetMatchMask(Vector128<T> left, Vector128<T> right) {
                 return Vector128s.OnesComplement(Vector128Helper.Equals(left, right));
             }
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 Vector256<T> GetMatchMask(Vector256<T> left, Vector256<T> right) {
                 return Vector256s.OnesComplement(Vector256Helper.Equals(left, right));
             }
 #endif // NETCOREAPP3_0_OR_GREATER
 #if NET8_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static Vector512<T> GetMatchMask(Vector512<T> left, Vector512<T> right) {
                 return ~Vector512.Equals(left, right);
             }
 #endif // NET8_0_OR_GREATER
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public
 #if GENERIC_MATH
-                static
+                    static
 #endif // GENERIC_MATH
                 Vector<T> GetMatchMask(Vector<T> left, Vector<T> right) {
                 return ~Vector.Equals(left, right);
