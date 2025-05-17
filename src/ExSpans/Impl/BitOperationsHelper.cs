60aca1f4c4297d521bc78df16416a09bf6e9f4ec
zhouyuelin <as.zhouyuelin@aisino.com>
2025-05-17 18:28:37 +0800

    ExSpans: BitOperationsHelper add FlipBit methods.

--- src/ExSpans/Impl/BitOperationsHelper.cs
+++ src/ExSpans/Impl/BitOperationsHelper.cs
@@ -29,5 +29,32 @@ namespace Zyl.ExSpans.Impl {
             return value & (value - 1);
         }
 
+        /// <summary>
+        /// Flip the bit at a specific position in a given value.
+        /// Similar in behavior to the x86 instruction BTC (Bit Test and Complement).
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="index">The zero-based index of the bit to flip.
+        /// Any value outside the range [0..31] is treated as congruent mod 32.</param>
+        /// <returns>The new value.</returns>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint FlipBit(uint value, int index) {
+            return value ^ (1u << index);
+        }
+
+        /// <summary>
+        /// Flip the bit at a specific position in a given value.
+        /// Similar in behavior to the x86 instruction BTC (Bit Test and Complement).
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="index">The zero-based index of the bit to flip.
+        /// Any value outside the range [0..63] is treated as congruent mod 64.</param>
+        /// <returns>The new value.</returns>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong FlipBit(ulong value, int index) {
+            return value ^ (1ul << index);
+        }
     }
 }
