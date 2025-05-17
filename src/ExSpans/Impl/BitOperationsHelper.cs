e14a24cf2b4afb0907df66988291ef53b2141183
zhouyuelin <as.zhouyuelin@aisino.com>
2025-05-17 18:02:33 +0800

    ExSpans: Add BitOperationsHelper class. Add ResetLowestSetBit methods.

---
+++ src/ExSpans/Impl/BitOperationsHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Zyl.ExSpans.Impl {
+    /// <summary>
+    /// <see cref="BitOperations"/> Helper.
+    /// </summary>
+    public static class BitOperationsHelper {
+
+        /// <summary>
+        /// Reset the lowest significant bit in the given value
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>Returns reset the lowest significant bit in the given value.</returns>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint ResetLowestSetBit(uint value) {
+            // It's lowered to BLSR on x86
+            return value & (value - 1);
+        }
+
+        /// <inheritdoc cref="ResetLowestSetBit(uint)"/>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong ResetLowestSetBit(ulong value) {
+            // It's lowered to BLSR on x86
+            return value & (value - 1);
+        }
+
+    }
+}
