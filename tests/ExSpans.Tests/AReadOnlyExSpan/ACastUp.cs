3516d20b4c03a3ae9cdbf546191920011511fa31
zhouyuelin <as.zhouyuelin@aisino.com>
2025-05-18 17:56:00 +0800

    ExSpans.Tests: Implements ACastUp class.

--- tests/ExSpans.Tests/AReadOnlyExSpan/ACastUp.cs
+++ tests/ExSpans.Tests/AReadOnlyExSpan/ACastUp.cs
@@ -2,11 +2,13 @@ using Xunit;
 
 namespace Zyl.ExSpans.Tests.AReadOnlyExSpan {
     public static partial class ACastUp {
+#if NET7_0_OR_GREATER
         [Fact]
         public static void CastUp() {
             ReadOnlyExSpan<string> strings = new string[] { "Hello", "World" };
             ReadOnlyExSpan<object> span = ReadOnlyExSpan<object>.CastUp(strings);
             span.ValidateReferenceType("Hello", "World");
         }
+#endif // NET7_0_OR_GREATER
     }
 }
