754c0ba44cd85f9ad15db4712fe561e83c2016d0
zhouyuelin <as.zhouyuelin@aisino.com>
2025-05-25 18:53:58 +0800

    Compatible.System: Add IsExternalInit class.

---
+++ src/Compatible.System/Runtime/CompilerServices/IsExternalInit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace System.Runtime.CompilerServices {
+#if NET5_0_OR_GREATER
+#else
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+#if SYSTEM_PRIVATE_CORELIB
+    public
+#else
+    internal
+#endif
+    static class IsExternalInit {
+    }
+#endif // NET5_0_OR_GREATER
+}
