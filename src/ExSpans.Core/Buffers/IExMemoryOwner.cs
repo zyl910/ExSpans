a164323ab523c9f25dbd08ac01225f08ee0b9ff4
zyl910 <zyl910@sina.com>
2025-06-22 23:28:55 +0800

    feat: Add IExMemoryOwner interface. #1

---
+++ src/ExSpans.Core/Buffers/IExMemoryOwner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Buffers;
+
+namespace Zyl.ExSpans.Buffers {
+    /// <summary>
+    /// Owner of <see cref="ExMemory{T}"/> that is responsible for disposing the underlying memory appropriately
+    /// (负责适当地释放基础内存的 <see cref="ExMemory{T}"/> 所有者).
+    /// </summary>
+    /// <typeparam name="T">The element type (元素的类型).</typeparam>
+    public interface IExMemoryOwner<T> : IMemoryOwner<T>, IDisposable {
+        /// <summary>
+        /// Returns a <see cref="ExMemory{T}"/>.
+        /// </summary>
+        ExMemory<T> ExMemory { get; }
+    }
+}
