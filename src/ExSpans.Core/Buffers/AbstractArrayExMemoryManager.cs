fa26e40cdc222c348d62a1fd6d37d7a85195daa2
zyl910 <zyl910@sina.com>
2025-06-28 16:55:23 +0800

    feat: Add MemoryAllocFlags enum. #3

--- src/ExSpans.Core/Buffers/AbstractArrayExMemoryManager.cs
+++ src/ExSpans.Core/Buffers/AbstractArrayExMemoryManager.cs
@@ -8,6 +8,26 @@ using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Zyl.ExSpans.Buffers {
+
+    /// <summary>
+    /// Memory alloc flags (内存分配标志).
+    /// </summary>
+    [Flags]
+    public enum MemoryAllocFlags {
+        /// <summary>None (无).</summary>
+        None = 0,
+        /// <summary>Zero the data when alloc. This flag will come with a small amount of overhead (分配时将数据清零. 该标志会来带少量开销).</summary>
+        ClearAlloc = 1,
+        /// <summary>Zero the data when free. This flag will come with a small amount of overhead (释放时将数据清零. 该标志会来带少量开销).</summary>
+        ClearFree = 2,
+        /// <summary>Keep old data when realloc. This flag will come with a large amount of overhead (重新分配时保留旧数据. 该标志会来带大量开销).</summary>
+        KeepRealloc = 4,
+        /// <summary>No set memory pressure. Without this flag, call <see cref="GC.AddMemoryPressure(long)"/> method when unmanaged memory allocated, and call <see cref="GC.RemoveMemoryPressure(long)"/> method when unmanaged memory freed. (不设置内存压力. 没有此标志时, 分配非托管内存时会调用 AddMemoryPressure 方法, 释放非托管内存时会调用 RemoveMemoryPressure 方法).</summary>
+        NoPressure = 8,
+        /// <summary>Manual trim. When the length decreases, the TrimExcess method is generally not automatically called and needs to be manually called (手动修剪. 当长度变小时, 一般不会自动调用 TrimExcess 方法 , 需要手动调用该方法).</summary>
+        TrimManual = 0x10,
+    }
+
     /// <summary>
     /// Array pool based abstract memory manager. It will rent the array from the array pool based on the length parameter, and return array to the array pool when when Dispose
     /// (基于数组池的抽象内存管理者. 它会根据长度参数, 从数组池中租借数组, 并在 Dispose 时归还数组到数组池).
@@ -16,6 +36,7 @@ namespace Zyl.ExSpans.Buffers {
     public abstract class AbstractArrayExMemoryManager<T> : ExMemoryManager<T> {
         private nint _length;
         private ArrayPool<T>? _pool;
+        private MemoryAllocFlags _flags;
         private T[]? _dataArray;
         private bool _isDisposed = false;
 
@@ -24,9 +45,10 @@ namespace Zyl.ExSpans.Buffers {
         /// </summary>
         /// <param name="length">Length of unmanaged data (非托管数据的长度).</param>
         /// <param name="pool">The <see cref="ArrayPool{T}"/> instance used to rent array. Defaults to <see cref="ArrayPool{T}.Shared"/> if it is null (用于租用数组的 <see cref="ArrayPool{T}"/> 实例. 它为空时默认为 <see cref="ArrayPool{T}.Shared"/>).</param>
+        /// <param name="flags">Memory alloc flags (内存分配标志). This class supports these flags: <see cref="MemoryAllocFlags.ClearAlloc"/>, <see cref="MemoryAllocFlags.ClearFree"/>.</param>
         /// <exception cref="ArgumentOutOfRangeException">The length parameter must be greater than or equal to 0. The length parameter out of array max length.</exception>
         [CLSCompliant(false)]
-        protected AbstractArrayExMemoryManager(nint length, ArrayPool<T>? pool = null) {
+        protected AbstractArrayExMemoryManager(nint length, ArrayPool<T>? pool = null, MemoryAllocFlags flags = default) {
             // Check.
             if (length < 0) {
                 throw new ArgumentOutOfRangeException(nameof(length), "The length parameter must be greater than or equal to 0.");
@@ -46,6 +68,10 @@ namespace Zyl.ExSpans.Buffers {
             _dataArray = pool.Rent((int)length);
             _length = length;
             _pool = pool;
+            _flags = flags;
+            if (_length > 0 && Flags.HasFlag(MemoryAllocFlags.ClearAlloc)) {
+                GetExSpan().Clear();
+            }
         }
 
         /// <summary>
@@ -57,7 +83,8 @@ namespace Zyl.ExSpans.Buffers {
             _isDisposed = true;
             // free.
             if (_dataArray is not null && _pool is not null) {
-                _pool.Return(_dataArray);
+                bool clearArray = Flags.HasFlag(MemoryAllocFlags.ClearFree);
+                _pool.Return(_dataArray, clearArray);
                 _dataArray = null;
             }
         }
@@ -113,6 +140,10 @@ namespace Zyl.ExSpans.Buffers {
         protected T[]? DataArray {
             get => _dataArray;
             set => _dataArray = value;
+        }
+        /// <summary>Memory alloc flags (内存分配标志).</summary>
+        public MemoryAllocFlags Flags {
+            get => _flags;
         }
 
         /// <summary>Is it Disposed (是否已处置).</summary>
